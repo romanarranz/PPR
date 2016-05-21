9e21c3713c048c22cf655e4cab4cdf68fdc1c332
roman <burial92@gmail.com>
2016-05-21 16:20:53 +0200

    casi terminado 2

--- P3/cuda/floydSharedMCUDA.cu
+++ P3/cuda/floydSharedMCUDA.cu
@@ -6,38 +6,48 @@ using std::endl;
 
 #include "floyd.h"
 
-#define CUDA_CHECK(call) \
+/*#define CUDA_CHECK(call) \
     if((call) != cudaSuccess) { \
         cudaError_t err = cudaGetLastError(); \
         cerr << "CUDA error calling \""#call"\", code is " << err << endl; }
+*/
+
+#define CUDA_CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=false)
+{
+    if (code != cudaSuccess){
+        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
 
 // Kernel to update the Matrix at k-th iteration
 extern __shared__ int smem[];
 __global__ void floyd1DSharedMKernel(int * M, const int nverts, const int k, const int blockSize){
     int li = threadIdx.x;                             // indice local en el vector de memoria compartida (shared)
-    int ii = blockIdx.x * blockDim.x + threadIdx.x;   // indice filas en el vector de memoria global
+    int ii = blockIdx.x * blockDim.x + threadIdx.x;   // coincide con ij, indice filas en el vector de memoria global
     int i = ii/nverts;
     int j = ii - (i*nverts);
 
     // vectores de memoria compartida
-    int* s_rowI = &smem[0];
-    int* s_rowK = &smem[blockSize];
+    int *s_rowI = (int *) &smem; //a is manually set at the beginning of shared
+    int *s_rowK = (int *) &s_rowI[blockSize/2]; //b is manually set at the end of a
 
     // cargar los datos al vector de memoria compartida
-    // int ij = (i*nverts) + j; = ii asi que no es necesario
     int kj = (k*nverts) + j;
     s_rowI[li] = M[ii];
     s_rowK[li] = M[kj];
     __syncthreads();
-    printf("TID = %u \n\tI = %u => \tS_I[%u] = %u \t? M[%u] = %u \n \tK = %u => \tS_K[%u] = %u \t? M[%u] = %u  \n", ii, i, li, s_rowI[li], ii, M[ii], k, li, s_rowK[li], kj, M[kj]);
-    
+    // printf("TID = %u \n\tI = %u => \tS_I[%u] = %u \t? M[%u] = %u \n \tK = %u => \tS_K[%u] = %u \t? M[%u] = %u  \n", ii, i, li, s_rowI[li], ii, M[ii], k, li, s_rowK[li], kj, M[kj]);
+
     // realizar el calculo
     if(i < nverts && j < nverts){
-        if (i!=j && i!=k && j!=k) {
-            int ik = (k*nverts) - (ii + k) - 1;
-            int aux = s_rowI[ik] + s_rowK[li];
+        if (i!=j && i!=k && j!=k){
 
-            int vikj = min(aux, M[li]);
+            int aux = s_rowI[k] + s_rowK[li];
+
+            // printf("(k,i,j) -> (%u,%u,%u)\n\t ik=%u, kj=%u ij=%u\n", k, i, j, s_rowI[k], s_rowK[li], s_rowI[li]);
+            int vikj = min(aux, s_rowI[li]);
             M[ii] = vikj;
         }
     }
@@ -63,7 +73,7 @@ void floyd1DSharedMGPU(int * h_M, int N, int numBloques, int numThreadsBloque){
     cout << "GPU: Calculando..." << endl;
     dim3 nblocks(numBloques);
     dim3 threadsPerBlock(numThreadsBloque, 1);
-    int blockSize = numBloques * 2;
+    int blockSize = numThreadsBloque * 2;
     for(int k = 0; k < N; k++){
         floyd1DSharedMKernel<<< nblocks, threadsPerBlock, blockSize * sizeof(int) >>> (d_M, N, k, blockSize );
     }
