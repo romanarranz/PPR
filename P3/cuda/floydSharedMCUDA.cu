cc0a84d2e5937ba44be84fa5ed2aab4952bdd34d
romanarranz <burial92@gmail.com>
2016-05-31 13:10:09 +0200

    esqueleto floyd2D Shared

--- P3/cuda/floydSharedMCUDA.cu
+++ P3/cuda/floydSharedMCUDA.cu
@@ -64,7 +64,75 @@ __global__ void floyd1DSharedMKernel(int * M, const int nverts, const int k, con
     }
 }
 
+extern __shared__ int smem[];
+__global__ void floyd2DSharedMKernel(int * M, const int nverts, const int k, const int blockSize){
+    // <== INICIALIZACION
+    // ====================================>
+    int li = threadIdx.x;
+    int lj = threadIdx.y;
+    int ii = blockIdx.x * blockDimm.x + threadIdx.x;
+    int i = ii/nverts;
+    int j = ii - (i*nverts);
+
+    // <== PREPARAR DATOS
+    // ====================================>
+    // vectores de memoria compartida
+    int * s_rowK = (int *) &smem;                    // filak del bloque
+    int * s_colK = (int *) &s_rowI[blockSize/2];     // columnak del bloque
+
+    // cargar los datos al vector de memoria compartida
+    int ik = (k*nverts) + j;
+    int kj = (i*nverts) + k;
+    s_rowK[li] = M[ik];
+    s_colK[li] = M[kj];
+
+    __syncthreads();
+
+    // <== CALCULO
+    // ====================================>
+    // fila de la primera hebra del bloque
+    int rowFirstBTid = floor((double) (blockIdx.x * blockDim.x) / nverts);
+
+    if(i < nverts && j < nverts){
+        if (i!=j && i!=k && j!=k){
+            M[ii] = min(s_ik[0] + s_rowK[li], s_rowI[li]);
+        }
+    }
+}
+
 void floyd1DSharedMGPU(int * h_M, int N, int numBloques, int numThreadsBloque){
+    // Compute Capability             1.x    2.x - 3.x
+    // ---------------------------------------------------
+    // Threads per block              512    1024
+    // Max shared memory (per block)  16KB   48KB
+
+    unsigned int sizeMatrix = N * N;
+    unsigned int memSize = sizeMatrix * sizeof(int);
+
+    // GPU variables
+    int * d_M = NULL;
+
+    CUDA_CHECK(cudaMalloc((void **)&d_M, memSize));
+
+    cout << "CPU: Copiando las matrices de la CPU RAM a la GPU DRAM..." << endl;
+    CUDA_CHECK(cudaMemcpy(d_M, h_M, memSize, cudaMemcpyHostToDevice));
+
+    cout << "GPU: Calculando..." << endl;
+    dim3 nblocks(numBloques);
+    dim3 threadsPerBlock(numThreadsBloque, 1);
+    int blockSize = numThreadsBloque * 2;
+    for(int k = 0; k < N; k++){
+        floyd1DSharedMKernel<<< nblocks, threadsPerBlock, blockSize * sizeof(int) >>> (d_M, N, k, blockSize );
+    }
+
+    cout << "CPU: Copiando los resultados de la GPU DRAM a la CPU RAM..." << endl;
+    CUDA_CHECK(cudaMemcpy(h_M, d_M, memSize, cudaMemcpyDeviceToHost));
+
+    // Flush all profile data before the application exits
+    cudaDeviceReset();
+}
+
+void floyd2DSharedMGPU(int * h_M, int N, int numBloques, int numThreadsBloque){
     // Compute Capability             1.x    2.x - 3.x
     // ---------------------------------------------------
     // Threads per block              512    1024
---
+++ P3/src/floydParalelo2DShared.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <fstream>
+#include <string.h>
+#include <time.h>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::min;
+
+#include "cuda_runtime.h"
+
+#include "Graph.h"
+#include "floyd.h"
+
+// Error handling macro
+#define CUDA_CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=false)
+{
+    if (code != cudaSuccess){
+        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
+
+void copiaGrafo(int * h_M, Graph g, int N){
+    for(int i = 0; i<N; i++)
+        for(int j = 0; j<N; j++)
+            h_M[i * N + j] = g.arista(i,j);
+}
+
+int main(int argc, char **argv){
+
+    if (argc != 2) {
+        cerr << "Sintaxis: " << argv[0] << " <archivo de grafo>" << endl;
+	    return(-1);
+	}
+
+    // Get Device Information
+    int devID = 0;
+    CUDA_CHECK(cudaSetDevice(devID));
+    CUDA_CHECK(cudaGetDevice(&devID));
+
+    cudaDeviceProp deviceProp;
+    CUDA_CHECK(cudaGetDeviceProperties(&deviceProp, devID));
+    if (deviceProp.computeMode == cudaComputeModeProhibited){
+        cerr << "Error: La GPU no permite realizar computo ahora mismo, las hebras no pueden usar ::cudaSetDevice()." << endl;
+        exit(EXIT_SUCCESS);
+    }
+    else{
+        cout << "GPU Device " << devID << ": \"" << deviceProp.name << "\" with compute capability " << deviceProp.major << "." << deviceProp.minor << endl << endl;
+    }
+
+    // bloque de 256 * 1 = 256 threads en el bloque
+    short blockS = 3;
+
+    // CPU variables
+    Graph G;
+    G.lee(argv[1]);
+    // G.imprime();
+
+    const unsigned int N = G.vertices;
+    const unsigned int sizeMatrix = N * N;
+    const unsigned int memSize = sizeMatrix * sizeof(int);
+    int * h_M = (int *) malloc(memSize);
+    copiaGrafo(h_M, G, N);
+
+    // comprobar si es divisible el tamaño de la matriz entre el tamaño del bloque
+    if( N % blockS != 0) blockS = 16;
+
+    dim3 blockSize(blockS, 1);  // el bloque de 256 * 1 en 1D
+    int numBloques = ceil((float) sizeMatrix / blockSize.x);
+    int numThreadsBloque = blockSize.x;
+
+    cout << "El blockSize es de: " << blockS << endl;
+    cout << "El numBloques es de: " << numBloques << endl;
+    cout << "El numThreadsBloque es de: " << numThreadsBloque << endl << endl;
+
+    // Calc
+    double t1 = clock();
+    floyd2DSharedMGPU(h_M, N, numBloques, numThreadsBloque);
+    double Tgpu = clock();
+    Tgpu = (Tgpu-t1)/CLOCKS_PER_SEC;
+
+    cout << "CPU: Mostrando resultados..." << endl;
+    // cout << endl << "El Grafo con las distancias de los caminos más cortos es:" << endl << endl;
+    // G.imprime();
+    cout << "Tiempo gastado GPU = " << Tgpu << endl << endl;
+
+    // Comprobar si los resultados de CPU y GPU coinciden
+    cout << "Comprobando resultados..." << endl;
+    for(int k=0;k<N;k++){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if (i!=j && i!=k && j!=k) {
+                    int vikj=min(G.arista(i,k)+G.arista(k,j),G.arista(i,j));
+                    G.inserta_arista(i,j,vikj);
+                }
+            }
+        }
+    }
+
+    bool error = false;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if ( abs(h_M[i * N + j] - G.arista(i,j)) > 0 ){
+                // cout <<"Error ("<<i<<","<<j<<")   " << h_M[i * N + j] << "..." << G.arista(i,j) << endl;
+                error = true;
+            }
+        }
+    }
+    if(error){
+        int i,j,vij;
+        for(i = 0; i < N; i++){
+            cout << "A[" << i << ",*]= ";
+
+            for(j=0;j<N;j++){
+                if (h_M[i*N+j]==INF)
+                    cout << "INF";
+                else
+                    cout << h_M[i*N+j];
+
+                if (j<N-1)
+                    cout << ",";
+                else
+                    cout << endl;
+            }
+        }
+
+        cout <<"With ERRORS" << endl;
+    }
+    else cout << "ALL OK" << endl;
+
+    // Liberando memoria de CPU
+    free(h_M);
+}
